e6bae53acc5d077ba22324bf19174e9a28985873
hanseonghye <hans12312@naver.com>
2020-03-16 23:28:33 +0900

    add chapter 31

--- chapter26/src/test/java/demo/SampleControllerTest.java
+++ chapter26/src/test/java/demo/SampleControllerTest.java
@@ -1,7 +1,5 @@
 package demo;
 
-import demo.sample.SampleController;
-import demo.sample.SampleService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
